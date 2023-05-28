f88aea7c5d96acec596448b41bc6bcd7e6fd6ab6
Avdiienko Illia <avil48job@gmail.com>
2023-05-28 11:04:33 +0300

    test(subscribe): create some tests cases that do fail yet

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+from os import path
+from os import remove as remove_file
+
+from pytest import fixture
+
+
+@fixture
+def sample_db():
+    create_test_db_instance()
+
+    yield
+
+    teardown_test_db_instance()
+
+
+def create_test_db_instance():
+    with open('db.csv', 'w') as file:
+        ...
+
+
+def teardown_test_db_instance():
+    if path.exists('db.csv'):
+        remove_file('db.csv')
---
+++ tests/test_subscribe.py
@@ -0,0 +1,33 @@
+import random
+import string
+
+from assertpy import assert_that
+
+from subscribe import subscribe, _retrieve_all_email_records
+
+
+def generate_sample_string(length: int = 8):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def test_writing_100_values(sample_db):
+    emails = [generate_sample_string() for _ in range(100)]
+
+    for email in emails:
+        subscribe(email)
+
+    retrieved = _retrieve_all_email_records()
+
+    assert_that(retrieved).is_type_of(list)
+    assert_that(retrieved).is_length(100)
+
+
+def test_writing_already_existing_value_raises_error(sample_db):
+    emails = [generate_sample_string() for _ in range(10)]
+
+    for email in emails:
+        subscribe(email)
+
+    email_that_is_already_written = random.choice(emails)
+
+    assert_that(subscribe).raises(ValueError).when_called_with(email_that_is_already_written)
