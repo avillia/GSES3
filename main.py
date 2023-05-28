785055b10d700260bfd45629acaa1b95c6bac714
Avdiienko Illia <avil48job@gmail.com>
2023-05-28 11:43:30 +0300

    feat(app): introduce exception handler for app that creates response based on exception

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import Response
 from pydantic import EmailStr
 
 from btc_rate import fetch_btc_rate
-
+from subscribe import subscribe
+from utils import setup_db
 
 app = FastAPI()
+setup_db()
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(
+    request: Request,
+    exception: HTTPException,
+) -> Response:
+    return Response(
+        status_code=exception.status_code,
+        content=exception.detail,
+    )
 
 
 @app.get("/rate", tags=["rate"])
